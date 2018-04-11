74a244ec3ff4708dc4ece9a52a240d93088ad887
Ming Ming Zhang <38026940+DrMMZ@users.noreply.github.com>
2018-04-11 12:14:58 -0400

    Update AttackDH.py

--- AttackDH.py
+++ AttackDH.py
@@ -1,10 +1,12 @@
+# I. Cryptanalysis of the Diffie-Hellman protocol.
+
 print()
 print("---------------------------------------------------------------")
 print("    Cryptanalysis of the Diffie-Hellman public key protocol    ")
 print("---------------------------------------------------------------")
 print()
 
-# Collect public information, i.e., the prime, generator and sendings.
+# 1. Collect public information, i.e., the prime, generator and sendings.
 p = input("Confirm the prime: ")
 p = int(p)
 g = input("Confirm the generator: ")
@@ -16,7 +18,7 @@ B = input("B sent: ")
 B = int(B) # B = g^{b} mod p where b is the secret integer of Bob
 print()
 
-# Find an integer x in [1,p-1] s.t. g^{x} = A mod p and g^{x} = B mod p.
+# 2. Find an integer x in [1,p-1] s.t. g^{x} = A mod p and g^{x} = B mod p.
 # Note that the complexity of this computation is exponential as it is the discrete logarithm problem.
 for x in range(1,p) :
     if (g**x)%p == A :
@@ -29,7 +31,7 @@ print("Password for A:",a)
 print("Password for B:",b)
 print()
 
-# Compute the secrete common key.
+# 3. Compute the secrete common key.
 k_a = (B**a)%p
 k_b = (A**b)%p
 #print(k_a,k_b)
